96668af873557007c831b29f96cbc81494b26ba6
Dragomir Urdov <dragomir.urdov@gmail.com>
2021-05-01 22:15:22 +0200

    #6 removed unnecesery code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
-import { environment } from 'environments/environment';
-
-import * as AppGlobals from 'app.globals';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +6,7 @@ import * as AppGlobals from 'app.globals';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private http: HttpClient) {
-    // !Ony for testing purpose! Remove after implementing APIs
-    this.http.get(environment.host).subscribe((result) => console.log(result));
-  }
+  constructor() {}
 
   ngOnInit() {}
 }
