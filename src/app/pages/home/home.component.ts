0530126d0d773ad5bb61b549fe97e91e64bcd693
DESKTOP-U4FHAFT\PC2 <nikolabaltic036@gmail.com>
2021-05-15 00:35:11 +0200

    #9 Slideshow/carousel implementation in homepage.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,12 +4,38 @@ import { routeAnimation } from '@app/shared/animations';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  constructor() {}
+
+  public sliderImage: number = 1;
+
+  constructor() {
+    this.slideshowTimer();
+  }
 
   ngOnInit(): void {}
+
+  /**
+  * It changes slides every 5 seconds.
+  */
+  slideshowTimer(){
+    if (this.sliderImage < 4) {
+      this.sliderImage = this.sliderImage + 1;
+    } else {
+      this.sliderImage = 1;
+    }
+    setTimeout(()=>{
+      this.slideshowTimer()  
+    }, 5000);
+  }
+
+  /**
+  * It selects a slide in the slideshow.
+  */
+  selectSlideshowImage(img: number){
+    this.sliderImage = img;
+  }
 
   ngOnDestroy(): void {}
 }
