b2f70f6003982354dbd299f4f6f6bdc243eb71f3
Cedomir Perisic <cedomirperisic@gmail.com>
2021-05-27 22:29:11 +0200

    adding new component items card

---
+++ src/app/shared/components/items-card/items-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-items-card',
+  templateUrl: './items-card.component.html',
+  styleUrls: ['./items-card.component.scss'],
+})
+export class ItemsCardComponent implements OnInit {
+  // itemsImg = '/assets/pictures/image-10.JPG';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
