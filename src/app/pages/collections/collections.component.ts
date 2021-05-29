81a836a6899b5ca4d9a275113322a918b37d9f51
Cedomir Perisic <cedomirperisic@gmail.com>
2021-05-29 20:24:25 +0200

    adding new component and module about and implementing collection page card

--- src/app/pages/collections/collections.component.ts
+++ src/app/pages/collections/collections.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { routeAnimation } from '@app/shared/animations';
+import { Collections } from '@app/shared/models';
+import { CommonService } from '@app/shared/services';
 
 @Component({
   selector: 'app-collections',
@@ -7,7 +9,15 @@ import { routeAnimation } from '@app/shared/animations';
   styleUrls: ['./collections.component.scss'],
 })
 export class CollectionsComponent implements OnInit, OnDestroy {
-  constructor() {}
+  collectionImg = '/assets/pictures/image-10.JPG';
+  collectionLoo2020 = 'Loo-2020';
+  collectionLoo2021 = 'Loo-2021';
+
+  collections: Collections[];
+
+  constructor(private commonService: CommonService) {
+    this.collections = this.commonService.config.collections;
+  }
 
   ngOnInit(): void {}
 
